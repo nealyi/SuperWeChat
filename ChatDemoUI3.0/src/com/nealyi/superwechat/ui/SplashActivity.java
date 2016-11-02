52437576c6466ea083514ae78e173708a03b9e1e
nealyi <nealyi@163.com>
2016-11-02 21:49:29 +0800

    Complete saving user's information in memory when splash works.

--- ChatDemoUI3.0/src/com/nealyi/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/com/nealyi/superwechat/ui/SplashActivity.java
@@ -7,78 +7,87 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
 import com.nealyi.superwechat.SuperWeChatHelper;
 import com.nealyi.superwechat.R;
 import com.hyphenate.util.EasyUtils;
+import com.nealyi.superwechat.db.UserDao;
+import com.nealyi.superwechat.utils.L;
 
 /**
  * 开屏页
- *
  */
 public class SplashActivity extends BaseActivity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
 
-	private static final int sleepTime = 2000;
+    private static final int sleepTime = 2000;
+    SplashActivity mContext;
 
-	@Override
-	protected void onCreate(Bundle arg0) {
-		setContentView(R.layout.em_activity_splash);
-		super.onCreate(arg0);
+    @Override
+    protected void onCreate(Bundle arg0) {
+        setContentView(R.layout.em_activity_splash);
+        super.onCreate(arg0);
 
-		RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
-		TextView versionText = (TextView) findViewById(R.id.tv_version);
+        RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
+        TextView versionText = (TextView) findViewById(R.id.tv_version);
 
-		versionText.setText(getVersion());
-		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
-		animation.setDuration(1500);
-		rootLayout.startAnimation(animation);
-	}
+        versionText.setText(getVersion());
+        AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
+        animation.setDuration(1500);
+        rootLayout.startAnimation(animation);
+        mContext = this;
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-		new Thread(new Runnable() {
-			public void run() {
-				if (SuperWeChatHelper.getInstance().isLoggedIn()) {
-					// auto login mode, make sure all group and conversation is loaed before enter the main screen
-					long start = System.currentTimeMillis();
-					EMClient.getInstance().groupManager().loadAllGroups();
-					EMClient.getInstance().chatManager().loadAllConversations();
-					long costTime = System.currentTimeMillis() - start;
-					//wait
-					if (sleepTime - costTime > 0) {
-						try {
-							Thread.sleep(sleepTime - costTime);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					String topActivityName = EasyUtils.getTopActivityName(EMClient.getInstance().getContext());
-					if (topActivityName != null && (topActivityName.equals(VideoCallActivity.class.getName()) || topActivityName.equals(VoiceCallActivity.class.getName()))) {
-						// nop
-						// avoid main screen overlap Calling Activity
-					} else {
-						//enter main screen
-						startActivity(new Intent(SplashActivity.this, MainActivity.class));
-					}
-					finish();
-				}else {
-					try {
-						Thread.sleep(sleepTime);
-					} catch (InterruptedException e) {
-					}
-					startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
-					finish();
-				}
-			}
-		}).start();
+        new Thread(new Runnable() {
+            public void run() {
+                if (SuperWeChatHelper.getInstance().isLoggedIn()) {
+                    // auto login mode, make sure all group and conversation is loaed before enter the main screen
+                    long start = System.currentTimeMillis();
+                    EMClient.getInstance().groupManager().loadAllGroups();
+                    EMClient.getInstance().chatManager().loadAllConversations();
+                    UserDao dao = new UserDao(mContext);
+                    User user = dao.getUser(EMClient.getInstance().getCurrentUser());
+                    L.e(TAG, "user=" + user);
+                    SuperWeChatHelper.getInstance().setCurrentUser(user);
+                    long costTime = System.currentTimeMillis() - start;
+                    //wait
+                    if (sleepTime - costTime > 0) {
+                        try {
+                            Thread.sleep(sleepTime - costTime);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    String topActivityName = EasyUtils.getTopActivityName(EMClient.getInstance().getContext());
+                    if (topActivityName != null && (topActivityName.equals(VideoCallActivity.class.getName()) || topActivityName.equals(VoiceCallActivity.class.getName()))) {
+                        // nop
+                        // avoid main screen overlap Calling Activity
+                    } else {
+                        //enter main screen
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }
+                    finish();
+                } else {
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                    }
+                    startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
+                    finish();
+                }
+            }
+        }).start();
 
-	}
-	
-	/**
-	 * get sdk version
-	 */
-	private String getVersion() {
-	    return EMClient.getInstance().getChatConfig().getVersion();
-	}
+    }
+
+    /**
+     * get sdk version
+     */
+    private String getVersion() {
+        return EMClient.getInstance().getChatConfig().getVersion();
+    }
 }
